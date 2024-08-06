494fb58a79276455275c71763755f5f366865960
Reddy <nagendharreddykondapu@gmail.com>
2024-08-06 12:33:27 +0530

    table is added fetch all products table

--- src/app/fetchallproducts/fetchallproducts.component.ts
+++ src/app/fetchallproducts/fetchallproducts.component.ts
@@ -2,18 +2,21 @@ import { CommonModule } from '@angular/common';
 import { Component } from '@angular/core';
 import { ProductService } from '../services/product.service';
 import { RouterLink } from '@angular/router';
+import { MatTableModule } from '@angular/material/table';
 
 
 @Component({
   selector: 'app-fetchallproducts',
   standalone: true,
-  imports: [CommonModule,RouterLink],
+  imports: [CommonModule,RouterLink,MatTableModule],
   templateUrl: './fetchallproducts.component.html',
   styleUrl: './fetchallproducts.component.css'
 })
 export class FetchallproductsComponent {
 
   products:any
+
+  displayedColumns: string[] = ['id', 'productName', 'productPrice','view','edit','delete'];
 
   constructor(private productService : ProductService){
 
